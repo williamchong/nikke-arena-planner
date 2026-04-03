e07be7184673256f84873415675c4fccf26262f3
William Chong <me@williamchong.cloud>
2026-04-04 00:45:08 +0800

    🐛 Register CSS entry point so Tailwind/Nuxt UI styles load

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/ui',
     '@pinia/nuxt',
